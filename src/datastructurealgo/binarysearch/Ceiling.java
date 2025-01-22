70aa029d3252a5e36eefb3d67653da7be6e2535b
knomaeb <sunil.rana320778@gmail.com>
2025-01-22 19:54:14 +0530

    binary search

---
+++ src/datastructurealgo/binarysearch/Ceiling.java
@@ -0,0 +1,26 @@
+package datastructurealgo.binarysearch;
+
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 5;
+        int index = ceiling(nums, target);
+        System.out.println("index: " + index);
+    }
+    public static int ceiling(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
